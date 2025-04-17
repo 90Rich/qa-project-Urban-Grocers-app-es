b1ae4f80cf7298d92983c763f00d88ced28b7591
90Rich <ricardo_go08@hotmail.com>
2025-04-16 19:44:58 -0600

    cambio de URL

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-abff8e74-5607-47d4-9ec9-642c1bb989de.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-5d7468f0-bcb7-4118-81b7-1b4ee9f4f16d.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users/" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits/" # Almacena la ruta para crear un kit en esta variable
 USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
